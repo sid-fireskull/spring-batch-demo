3dea4645fbb45edaf4b713c1b30381662c9701cb
Sid <siddhartha@dcpltech.com>
2024-09-04 17:06:50 +0530

    SQL Error Fix

--- src/main/java/com/springbatch/demo/config/SpringBatchConfig.java
+++ src/main/java/com/springbatch/demo/config/SpringBatchConfig.java
@@ -1,6 +1,12 @@
 package com.springbatch.demo.config;
 
 import lombok.AllArgsConstructor;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -16,6 +22,7 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.item.json.JacksonJsonObjectReader;
 import org.springframework.batch.item.json.JsonItemReader;
 import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +31,9 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springbatch.demo.entity.Student;
 import com.springbatch.demo.repository.StudentRepository;
 
@@ -39,15 +49,30 @@ public class SpringBatchConfig {
     private StudentRepository customerRepository;
 
 
+//    @StepScope
+//    @Bean
+//    JsonItemReader<Student> jsonItemReader(@Value("#{jobParameters['students']}") String jsonFilePath) {
+//    	System.out.println("[+] File Path: "+ jsonFilePath);
+//        return new JsonItemReaderBuilder<Student>()
+//                .jsonObjectReader(new JacksonJsonObjectReader<>(Student.class))
+//                .resource(new FileSystemResource(jsonFilePath))
+//                .name("studentJsonItemReader")
+//                .build();
+//    }
+    
     @StepScope
     @Bean
-    JsonItemReader<Student> jsonItemReader(@Value("#{jobParameters['students']}") String studentJson) {
-    	//String filePath="";
-        return new JsonItemReaderBuilder<Student>()
-            .jsonObjectReader(new JacksonJsonObjectReader<>(Student.class))
-            .resource(new ByteArrayResource(studentJson.getBytes()))
-            .name("studentJsonItemReader")
-            .build();
+    ListItemReader<Student> listItemReader(@Value("#{jobParameters['students']}") String jsonFilePath)
+    {
+    	ObjectMapper objectMapper = new ObjectMapper();
+    	try {
+			List<Student> students = objectMapper.readValue(new File(jsonFilePath), new TypeReference<List<Student>>() {});
+			return new ListItemReader<>(students);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return new ListItemReader<Student>(new ArrayList<>());
     }
 
     @Bean
@@ -66,7 +91,7 @@ public class SpringBatchConfig {
     @Bean
     Step step1() {
         return stepBuilderFactory.get("import-step").<Student, Student>chunk(10)
-                .reader(jsonItemReader(null))
+                .reader(listItemReader(null))
                 .processor(processor())
                 .writer(writer())
                 .taskExecutor(taskExecutor())
--- src/main/java/com/springbatch/demo/controller/JobController.java
+++ src/main/java/com/springbatch/demo/controller/JobController.java
@@ -1,27 +1,18 @@
 package com.springbatch.demo.controller;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.File;
 import java.util.List;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springbatch.demo.entity.Student;
@@ -29,40 +20,26 @@ import com.springbatch.demo.entity.Student;
 @RestController
 public class JobController {
 
-    @Autowired
-    private JobLauncher jobLauncher;
-    @Autowired
-    private Job job;
+	@Autowired
+	private JobLauncher jobLauncher;
+	@Autowired
+	private Job job;
 
-    @PostMapping("/importCustomers")
-    public void importCsvToDBJob() {
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
-        try {
-            jobLauncher.run(job, jobParameters);
-        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
-            e.printStackTrace();
-        }
-    }
-    
 	@PostMapping("/addStudents")
-	public ResponseEntity<Void> insertAllStudent(@RequestBody  List<Student> students) throws JsonProcessingException
-	{
+	public ResponseEntity<Void> insertAllStudent(@RequestBody List<Student> students) throws JsonProcessingException {
 		ObjectMapper objectMapper = new ObjectMapper();
 
-		String studentJson = objectMapper.writeValueAsString(students);
-		JobParameters parameters = new JobParametersBuilder()
-										.addString("students", studentJson)
-										.toJobParameters();
-		
-		
-        try {
-            jobLauncher.run(job, parameters);
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-        	e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+		try {
+			File tempFile = File.createTempFile("students", ".json");
+			objectMapper.writeValue(tempFile, students);
+			JobParameters parameters = new JobParametersBuilder().addString("students", tempFile.getAbsolutePath())
+					.toJobParameters();
+			jobLauncher.run(job, parameters);
+			return ResponseEntity.ok().build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
-    
+
 }
